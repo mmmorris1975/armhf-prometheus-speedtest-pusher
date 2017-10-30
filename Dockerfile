95e6f3d18e29ddbeaafd10dfabe0803b56fdb522
Mike Morris <michael.m.morris@gmail.com>
2017-10-29 21:12:53 -0500

    update base image to official arm alpine, and install python ... keeps the image as small as possible.  Update build script to support pushing to repo

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
-FROM hypriot/rpi-python:latest
+FROM arm32v6/alpine:3.6
 MAINTAINER Mike Morris
 
-RUN pip install speedtest-cli prometheus_client
+RUN apk update && apk add python2 py2-pip && pip install speedtest-cli prometheus_client
 COPY collect_speedtest_metrics.py /collect_speedtest_metrics
 
 ENTRYPOINT [ "/collect_speedtest_metrics" ]
--- build.sh
+++ build.sh
@@ -1,6 +1,31 @@
 #!/bin/bash -x
 
-ver=${VERSION:="0.0.1"}
+ver=${VERSION:="0.0.2"}
 NAME=$(basename $(dirname $PWD/Dockerfile))
 
-docker build -t ${NAME}:${ver} -t ${NAME}:latest -t ${NAME}:$(date +%Y%m%d) .
+declare -a TAGS
+TAGS=($ver $(date +%Y%m%d))
+
+NAME=$(basename $(dirname $PWD/Dockerfile))
+
+if [ $REPO_HOST ]
+then
+  NAME=${REPO_HOST}/${NAME}
+fi
+
+docker build -t ${NAME}:latest $@ .
+
+if [ $REPO_HOST ]
+then
+  docker push ${NAME}:latest
+fi
+
+for i in ${TAGS[@]}
+do
+  docker tag ${NAME}:latest ${NAME}:$i
+
+  if [ $REPO_HOST ]
+  then
+    docker push ${NAME}:$i
+  fi
+done
